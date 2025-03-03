ee1301725d4264a5fd22c69c31d6905dc88034aa
Sameer876521 <samauvesh445@gmail.com>
2025-03-03 10:04:46 +0530

    update

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-console.log('Hello, World!')
+const btn = document.getElementById("btn");
+const container = document.getElementById("con");
 
-const url = "https://api.pokemontcg.io/v2/cards?q=name:pikachu";
-const mess = document.getElementById("mes")
+// Get name of Pokemon
+const getName = async () =>{
+  num = Math.floor(Math.random()*1000)
+console.log(num)
+  const url = `https://pokeapi.co/api/v2/pokemon/${num}`
+  
+  data = await fetch(url);
+  names = await data.json();
+  
+  val = names.name
+  
+  return val;
+}
 
+
+
+//Get the images of cards with
 const getdata = async() => {
   
+ const name = await getName();
+console.log(name)
+const url = `https://api.pokemontcg.io/v2/cards?q=name:${name}`;
 
 data = await fetch(url)
 
 img = await data.json();
-console.log(img.data.length)
 
+num = Math.floor(Math.random()*img.data. length) 
 
-for(i = 0; i < img.data.length; i++)
+image = document.createElement("img")
+image.src = img.data[num].images.large;
+container.prepend(image)
+
+/*for(i = 0; i < img.data.length; i++)
 {
   image = await document.createElement("img")
   image.src = await img.data[i].images.large
   await document.body.appendChild(image)
 }
-mess.innerHTML = "Completed!"
+*/
+
+
 }
 
-getdata();
+const getPokemon = () => {
+  
+}
+
+
+
+btn.addEventListener("click",getdata)
 
